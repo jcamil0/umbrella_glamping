6f010b627a63e73a3ffb26cfe4600ab28c0f9527
jcamil0 <jcamilo.llanos@hotmail.com>
2021-06-01 23:49:33 -0500

    post components added * without connection to firebase *

--- src/components/listData.js
+++ src/components/listData.js
@@ -1,44 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 
-class InputData extends Component {
-  state = {
-    msg: "",
-  };
+const ListData = (props) => {
+  return (
+    <div style={{ marginTop: 10 }}>
+      {props.listMsg.map((msg) => {
+        return <p key={msg.key}>{msg.message}</p>;
+      })}
+    </div>
+  );
+};
 
-  onTextChange = (e) => {
-    this.setState({
-      msg: e.target.value,
-    });
-  };
-
-  onClickButton = () => {
-    this.props.onClickButtonHandler(this.state.msg);
-  };
-
-  render() {
-    return (
-      <div className="panel-footer">
-        <div className="input-group">
-          <input
-            onChange={this.onTextChange}
-            value={this.state.msg}
-            id="btn-input"
-            type="text"
-            className="form-control input-sm"
-            placeholder="Type your message here..."
-          />
-          <span className="input-group-btn">
-            <button
-              className="btn btn-primary btn-sm"
-              onClick={this.onClickButton}
-              id="btn-chat"
-            >
-              Send
-            </button>
-          </span>
-        </div>
-      </div>
-    );
-  }
-}
-export default InputData;
+export default ListData;
